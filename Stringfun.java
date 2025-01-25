2172a9e1cd019d3f653cc71c91c222ea66ea5b3b
Ramyasri135 <kurvaramyasri4242@gmail.com>
2025-01-25 14:39:49 +0530

    Add files via upload

---
+++ Stringfun.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Stringfun {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner (System.in);
+        System.out.println("enter a string: ");
+        String a=sc.nextLine();
+        System.out.println("upper case of given  string : "+(a.toUpperCase()));
+    }   
+}
